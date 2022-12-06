fd568409d5b8c099433dc4529454daf365c76a81
Sofia <sayana-tv@ya.ru>
2022-12-06 12:07:02 +0800

    add Examples 005 Name if_else

---
+++ Example005_Name/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "софья")
+{
+    Console.WriteLine("Ура, это же Соня!");
+}
+else
+{
+   Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+Console.WriteLine("");
